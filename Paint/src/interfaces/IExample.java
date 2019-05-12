2ea61dfc4d429cee13ff9f981b13784f0d44c347
hienho <40543765+hohien@users.noreply.github.com>
2019-05-12 09:38:47 +0700

    set up project

--- Paint/src/interfaces/IExample.java
+++ Paint/src/interfaces/IExample.java
@@ -3,18 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package interfaces;
 
 /**
  *
  * @author mini03
  */
-public class Paint {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public interface IExample {
     
 }
